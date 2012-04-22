a860cc344ea8f63a4398b70dfce108c7469bb18c
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2012-04-22 21:08:35 +0000

    Added the px3json js Signed-off-by:Ivan Suftin <isuftin@usgs.gov>
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/js_lib/maven/openlayers-parent/trunk@21819 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ openlayers-px3json/src/main/webapp/js/openlayers/extension/Format/Px3JSON.js
@@ -0,0 +1,36 @@
+/* Copyright (c) 2006-2011 by OpenLayers Contributors (see authors.txt for 
+ * full list of contributors). Published under the Clear BSD license.  
+ * See http://svn.openlayers.org/trunk/openlayers/license.txt for the
+ * full text of the license. */
+
+
+/**
+ * Class: OpenLayers.Format.Px3JSON
+ */
+OpenLayers.Format.Px3JSON = OpenLayers.Class(OpenLayers.Format.JSON, {
+    
+    read: function(json, filter) {
+        var obj = null;
+        var requiredStringNode = 'defaultToolGroup';
+        
+        if (typeof json == "string") {
+            obj = OpenLayers.Format.JSON.prototype.read.apply(this, [json, filter]);
+        } else { 
+            obj = json;
+        }  
+        
+        if(!obj) {
+            OpenLayers.Console.error("Bad JSON: " + json);
+        } else if(typeof(obj[requiredStringNode]) != "string") {
+            OpenLayers.Console.error("Bad Px3JSON - no "+requiredStringNode+": " + json);
+        } else {
+            return obj;
+        }
+    },
+    
+    write: function(px3json, pretty) {
+        return OpenLayers.Format.JSON.prototype.write.apply(this, [px3json, pretty]);
+    },
+
+    CLASS_NAME: "OpenLayers.Format"
+});     
---
+++ openlayers-px3json/src/main/webapp/js/openlayers/openlayers-px3json.jsp
@@ -0,0 +1,2 @@
+
+<script type="text/javascript" src="${param['relPath']}js/openlayers/extension/Format/Px3JSON.js"></script>
